f1e3513f6697013cc382c116364d52ed5a36d3be
andlicon <andresjzabalac@hotmail.com>
2023-05-05 08:07:23 -0400

    ADD: jsx y css del Semaforo y fueron importados al proyecto

---
+++ src/js/component/Semaforo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../../styles/semaforo.css';
+
+const Semaforo = () => {
+    return(
+        <div className='semaforo-container'>
+            Semaforo
+        </div>
+    );
+};
+export default Semaforo;
--- src/js/page/Home.jsx
+++ src/js/page/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Semaforo from '../component/Semaforo.jsx';
+
 //create your first component
 const Home = () => {
 	return (
 		<>
+			<Semaforo />
 		</>
 	);
 };
